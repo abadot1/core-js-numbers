7bd8cd20c1ee8b9ca733cb7d4840074b29021928
abadot1 <medvedmladshy@yandex.by>
2023-11-14 23:14:26 +0300

    to 11

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -156,8 +156,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -177,8 +177,8 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return (num / 10 ** pow).toFixed(0) * 10 ** pow;
 }
 
 /**
